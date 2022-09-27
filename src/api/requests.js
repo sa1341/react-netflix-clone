b319d0a48561164951721bc5a7c3189066d9412c
임준영 <a79007714@gmail.com>
2022-09-27 23:45:20 +0900

    feat: themovie api 사용을 위한 request 정보 셋팅

---
+++ src/api/requests.js
@@ -0,0 +1,13 @@
+const requests = {
+    fetchNowPlaying: "movie/now_playing",
+    fetchNetflixOriginals: "/discover/tv?with_networks=213",
+    fetchTrending: "/trending/all/week",
+    fetchTopRated: "/movie/top_rated",
+    fetchActionMovies: "/discover/movie?with_genres=28",
+    fetchComedyMovies: "/discover/movie?with_generes=35",
+    fetchHorrorMovies: "/discover/movie?with_generes=27",
+    fetchRomanceMovies: "/discover/movie?with_generes=10749",
+    fetchDocumentaries: "/discover/movie?with_generes=99",
+}
+
+export default requests;
