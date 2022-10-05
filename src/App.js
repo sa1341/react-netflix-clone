850ee7e82c84f561110214602a688bf3ea2ac40d
임준영 <a79007714@gmail.com>
2022-10-05 23:32:09 +0900

    feat: netflix row 컴포넌트 예제작성

--- src/App.js
+++ src/App.js
@@ -1,12 +1,45 @@
-import './App.css';
+import requests from './api/requests';
+import "./App.css";
 import Banner from './components/Banner';
 import Nav from './components/Nav';
+import Row from './components/Row';
 
 function App() {
   return (
-    <div className="App">
+    <div className="app">
       <Nav /> 
       <Banner />
+      <Row 
+       title="NETFLIX ORIGINALS"
+       id="NO"
+       fetchUrl={requests.fetchNetflixOriginals}      
+       isLargeRow
+      />
+
+      <Row 
+       title="Trending Now"
+       id="TN"
+       fetchUrl={requests.fetchTrending}
+      />
+
+      <Row 
+       title="Top Rated"
+       id="TR"
+       fetchUrl={requests.fetchTopRated}
+      />
+
+      <Row 
+       title="Action movies"
+       id="AM"
+       fetchUrl={requests.fetchActionMovies}
+      />
+
+      <Row 
+       title="Comedy movies"
+       id="CM"
+       fetchUrl={requests.fetchComedyMovies}
+      />
+
     </div>
   );
 }
